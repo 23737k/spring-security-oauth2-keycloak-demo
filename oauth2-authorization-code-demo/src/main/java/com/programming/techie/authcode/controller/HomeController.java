5223336abf8302d09df4d257dc55b1d3f8465562
Kenti Antezana <mopup2017@gmail.com>
2024-08-06 14:31:38 -0300

    Agrego controller basico con thymeleaf

---
+++ oauth2-authorization-code-demo/src/main/java/com/programming/techie/authcode/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.programming.techie.authcode.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+
+
+    @GetMapping
+    public String redirect(){
+    return "redirect:/home";
+    }
+}
